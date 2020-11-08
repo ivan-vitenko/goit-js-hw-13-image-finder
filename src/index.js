a9a5d0c410f6c5f9524daa641151e6f6829acf3a
Ivan <vitanlhouse@gmail.com>
2020-11-08 08:38:28 +0200

    Додає робочий функціонал

--- src/index.js
+++ src/index.js
@@ -1,48 +1,104 @@
 import './styles.css';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
 import ImagesApiService from './apiService';
 import galleryTemplate from './templates/gallery.hbs';
+import largeImage from './templates/large-image.hbs';
 import Debounce from 'lodash.debounce';
+import * as basicLightbox from 'basiclightbox';
 
 const imagesApiService = new ImagesApiService();
 
 const refs = {
-  bodyEl: document.querySelector('body'),
-  inputEl: document.querySelector('.image-search-field'),
+  bodyEl: select('body'),
+  inputEl: select('.image-search-field'),
 };
 
 refs.inputEl.addEventListener('input', Debounce(onSearch, 500));
 
 function onSearch(event) {
-  //   clearOldSearchResults();
+  clearSearchResults();
 
   imagesApiService.query = event.target.value;
 
   if (imagesApiService.query) {
-    imagesApiService.fetchImages().then(createGallery);
+    createGallery();
+    try {
+      imagesApiService.fetchImages().then(addItemsInGallery);
+    } catch (error) {
+      alert(`${error}`);
+    }
+    setNextPage();
   }
 }
 
-function createGallery(gallery) {
-  if (countriesList.length > 10) {
-    error({
-      text: 'Результат пошуку надто великий. Зробіть запит більш унікальним!',
-    });
-    return;
+function createGallery() {
+  refs.bodyEl.insertAdjacentHTML('beforeend', '<ul class="gallery"></ul>');
+}
+
+function setNextPage() {
+  imagesApiService.page += 1;
+}
+
+function addItemsInGallery(gallery) {
+  select('.gallery').insertAdjacentHTML(
+    'beforeend',
+    galleryTemplate(gallery.hits),
+  );
+
+  select('.gallery').addEventListener('click', showLargeImage);
+
+  if (!select('.load-more-button') && gallery.total > 12) {
+    addLoadMoreButton();
   }
 
-  if (countriesList.length === 1) {
-    refs.bodyEl.insertAdjacentHTML('beforeend', countryTemplate(countriesList));
-    success({
-      text: 'Ура! Країну знайдено :)',
-    });
-    return;
-  }
+  scrollToEndPage();
+}
 
+function showLargeImage(event) {
+  basicLightbox
+    .create(
+      `<img src="${event.target
+        .closest('.gallery__item')
+        .querySelector('img')
+        .getAttribute('large-src')}">`,
+    )
+    .show();
+}
+
+function scrollToEndPage() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    left: 0,
+    behavior: 'smooth',
+  });
+}
+
+function select(element) {
+  return document.querySelector(element);
+}
+
+function addLoadMoreButton() {
   refs.bodyEl.insertAdjacentHTML(
     'beforeend',
-    countriesListTemplate(countriesList),
+    '<button class="load-more-button">Показати ще...</button>',
   );
-  info({
-    text: 'Виведено список країн за запитом.',
-  });
+
+  select('.load-more-button').addEventListener('click', showMoreImages);
+}
+
+function showMoreImages() {
+  imagesApiService.fetchImages().then(addItemsInGallery);
+  imagesApiService.page += 1;
+}
+
+function clearSearchResults() {
+  removeOldElement(document.querySelector('.gallery'));
+  removeOldElement(document.querySelector('.load-more-button'));
+  imagesApiService.page = 1;
+}
+
+function removeOldElement(element) {
+  if (element) {
+    element.remove();
+  }
 }
